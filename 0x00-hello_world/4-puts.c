1292cdce7c15b4b6dc24c82d8e31828b6ebe845f
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-09 09:34:09 -0700

    completed mandatory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - A description
+ *
+ *   * Return: integer
+ *
+ *    */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - A description
+ *
+ *   * Return: integer
+ *
+ *    */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
