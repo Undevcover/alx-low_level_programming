5a6966215c857f8bbf43bdb0b8d62b454f45c5dd
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-09 20:42:26 -0700

    correct errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-
-
 /**
- *
  *  * main - A description
- *
  *   * Return: integer
- *
  *    */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-
-
 /**
- *
  *  * main - A description
- *
  *   * Return: integer
- *
  *    */
-
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
