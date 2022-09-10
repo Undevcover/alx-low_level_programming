8ee04f8b537365eccdb8420b2bce318656cf1856
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-09 20:51:06 -0700

    add advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - A description
+ *   * Return: integer
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - \
+		      Dora Korpar, 2015-10-19\n";
+	fwrite(str, sizeof(char), sizeof(str) - 1, stderr);
+	fflush(stdout);
+	return (1);
+}
