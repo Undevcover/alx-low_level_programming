08738e111bc1da94ade825d4727c21ef06781cd6
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 08:45:26 -0700

    print single numbers using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-        int num;
+	int num;
 
-        for (num = 0; num < 10; num++)
-                putchar(num + '0');
-        putchar('\n');
-        return (0);
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
 }
