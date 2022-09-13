17b6ad51877757eacf2006f2d849982dadb67567
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 07:01:01 -0700

    to forgive, divine

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,12 @@
  
 int main(void) /* Returns void */
 {
-    char i;
-
-    for(i='a'; i<='z'; i++)
-    putchar("%c ",i);
- 
-    for(i='A'; i<='Z'; i++)
-        putchar("%c ",i);
- 
-     
-    return 0;
+	char i;
+	
+	for(i='a'; i<='z'; i++)
+		putchar("%c ",i);
+	
+	for(i='A'; i<='Z'; i++)
+		putchar("%c ",i);
+       return 0;
 }
