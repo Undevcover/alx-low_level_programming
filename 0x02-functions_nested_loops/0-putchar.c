f7f6753fbe8e565d7f92bbfe36a8b8104ddce457
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 16:15:35 -0700

    modified putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 /*  C program to print all single digit of hex*/
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main -Main function entry point
@@ -9,7 +10,7 @@ int main(void) /* The function returns void */
 {
 	char str[] = "_putchar";
 
-	_putchar( str);
+	_putchar str);
 	putchar('\n');
 	return (0);
 }
