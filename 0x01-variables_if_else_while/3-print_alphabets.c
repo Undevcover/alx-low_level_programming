1400665233954dadb0b428c6cf944984a9b67d58
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 07:04:22 -0700

    permission granted

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,7 @@
 /*C program to print all upper case and lower case alphabets.*/
  
 #include <stdio.h>
+
 /**
  * main -Main function entry point
  * Return: Always zero
@@ -10,10 +11,10 @@ int main(void) /* Returns void */
 {
 	char i;
 	
-	for(i='a'; i<='z'; i++)
+	for (i='a'; i<='z'; i++)
 		putchar("%c ",i);
 	
-	for(i='A'; i<='Z'; i++)
+	for (i='A'; i<='Z'; i++)
 		putchar("%c ",i);
        return 0;
 }
