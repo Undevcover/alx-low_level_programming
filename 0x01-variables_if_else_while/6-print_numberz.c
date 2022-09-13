a28ab6b9ba18595f1be63b5ab0fde43476ba3927
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 08:43:33 -0700

    print single numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/*  C program to ptint all single digit numbers using putchar */
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main(void)
+{
+        int num;
+
+        for (num = 0; num < 10; num++)
+                putchar(num + '0');
+        putchar('\n');
+        return (0);
+}
