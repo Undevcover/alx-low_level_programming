754c5cf43ad4067722b472d69cdc645b35a0badc
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 14:41:40 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char myarr[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(myarr[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
