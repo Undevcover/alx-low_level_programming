91d84dea4f3e545fe78007adb876630328eb96ba
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 13:43:56 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+/* C program to print an ugly word */
+#include <stdio.h>
+
+/**
+ * main - function entry point
+ * return: returns zero
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
