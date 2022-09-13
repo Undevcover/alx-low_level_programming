08992e641bc59655fe1124507758bd02d4a9ab61
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 05:22:15 -0700

    corret errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - A description
- *  * Return: integer
+ * main - Explanation of my code 
+ * Return: An integer
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" -\ Dora Korpar, 2015-10-19\n";
-	fwrite(str, sizeof(char), sizeof(str) - 1, stderr);
-	fflush(stdout);
-	return (1);
+char str[] = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n";
+fwrite(str, sizeof(char), sizeof(str) - 1, stderr);
+fflush(stdout);
+return (1);
 }
