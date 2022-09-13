3114a6a56782fdd27df4970ce153168fae2913aa
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 08:20:31 -0700

    print digits from0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/*  C program to ptint all single digit numbers of base 10 starting from 0, followed by a new line */
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main()
+{
+    int num;
+
+    for (num = 0; num <= '10'; num++)
+   
+        putchar(num);
+     
+    putchar('\n');
+    return (0);
+}
