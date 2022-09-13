f4185a816d7467403dc3a8c2cb055bbed897486b
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 05:38:07 -0700

    positive, negative or zero

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,19 @@
+/* Program to determine if a random number is positive, negative or zero */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: zero
+ */
+int main(void) /* returns void */
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
