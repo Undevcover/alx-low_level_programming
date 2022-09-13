8b6c1b912c31aa21fb117526cdb8e2682f71468e
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 08:51:06 -0700

    print all lowercase alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+/*  C program to print all lowecase alphabets in reverse*/
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = z; num <= 'a'; num--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
