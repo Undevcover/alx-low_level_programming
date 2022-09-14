40ee3347e07b727fc94eb7857727f5fb35d3236b
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 18:15:25 -0700

    serious attempt

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,14 @@
 
 void print_alphabet_x10(void)
 {
-	int letter;
-	int times;
+	int times, letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (times = 0; times <= 10; times++)
 	{
-		for (times = 0; times <= 10; times ++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 	}
-        _putchar ('\n');
-        
-}    
+	_putchar ('\n');
+}	
