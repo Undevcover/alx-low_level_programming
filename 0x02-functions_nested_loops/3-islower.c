7f226f5cc41ea01a278e135657d95754035c87dd
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-14 02:37:44 -0700

    add is lower c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * my_function - This is a description
+ */
+
+int _islower(int c) 
+{
+	int letter;
+	int c;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (c == letter)
+		{
+			_putchar(1)
+		}
+        _putchar (0);
+	}
+}
