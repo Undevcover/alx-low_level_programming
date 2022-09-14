2280259318687f63c62798d65bf1e34f69e616e7
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-13 17:27:36 -0700

    first attempt

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	int letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar (letter);
+	_putchar ('\n');
+}
