8484aa19940cdfd59e6f6777ed6037c77a820781
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-15 10:24:10 -0700

    first attempt

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include <main.h>
 
 /*
- *
- *
+ * Checks if alphabet is in uppercase
+ * Always returns void
  */
 int _isupper(int c)
 {
 	if (( c < 65)|| (c > 90)
 	{
-		return (1);
+		_putchar (1);
 	}
 	return (0);
 }
