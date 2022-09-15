25a13e9203d5b70c57951bfb0139ea5781a1f54d
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-15 13:31:40 -0700

    print numbers followed by a new line except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers -funtion that prints 0 - 9 followed by a new line
+ * except 2 and 4
+ * Return: always void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		if ((i == 2)||(i == 4))
+			continue;
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
