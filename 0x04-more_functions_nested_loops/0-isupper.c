c8ddcd73f1d38af6ef4f7e14469b55e9a16df357
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-16 02:46:04 -0700

    sorry, so sorty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
  *_isupper -  Checks if alphabet is in uppercase
  * @c: character to check
  * return 1 if c is uppercase, otherwise 0
  */
+
 int _isupper(int c)
 {
 	if ((c >= 'A')&&(c <= 'Z'))
