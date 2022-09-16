8dcc0b8208cce6651f7bb3050724cfdd5510fe70
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-15 23:48:05 -0700

    corret error

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
 
 void print_numbers(void)
 {
-        int i;
+	char i;
 
-        for (i = 0; i <= 9; i++)
-                _putchar(i + '0');
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 
-        _putchar('\n');
+	_putchar('\n');
 }
