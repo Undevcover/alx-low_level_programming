b8e57570b2ced10049b79d4985ee274dc3dc9055
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-16 04:34:13 -0700

    6th task solution

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,13 @@ void print_line(int n)
 	int i;
 	char space = '_';
 
-	for (i = 0; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		if (n <= 0)
 		{
-			_putchar('\n')
+			_putchar('\n');
 		}
 		_putchar(space);
 	}
+	  _putchar('\n');
 }
