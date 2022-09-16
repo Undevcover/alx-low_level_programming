62b6e0ac223378b4ce2cdcaff65d9594d20d724f
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-16 03:34:18 -0700

    first number 5 attempt

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /*
  * more_numbers - function thatb prints 10*
  * the numbers from 0-14
  * Return: zero
  */
- void more_numbers(void)
- {
-	 int i;
-	 int j;
+
+void more_numbers(void)
+{
+	int i;
+	int j;
 
 	for (i = 0; i <15; i++)
 	{
 		j= i*10;
-		_putchar(j);
+		_putchar(j + '0');
 	}
 	 _putchar('\n')
- }
+}
