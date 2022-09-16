e666bb08633104b0b08d21c075586321693b8aea
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-16 03:29:27 -0700

    first number 5 attempt

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * more_numbers - function thatb prints 10*
+ * the numbers from 0-14
+ * Return: zero
+ */
+ void more_numbers(void)
+ {
+	 int i;
+	 int j;
+
+	:wqfor (i = 0; i <15; i++)
+	{
+		j= i*10;
+		_putchar(j);
+	}
+	 _putchar('\n')
+ }
