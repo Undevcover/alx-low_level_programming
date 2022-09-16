9d52ee273890b4bac3d070ea5068ccc32f81e36a
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-16 04:17:06 -0700

    6th task solution

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
  /**
   * print_line- function that prints a line in the terminal
   * @n: number of lines
   * Return: printed 
   */
-  void print_line(int n)
-  {
-        int i;
+void print_line(int n)
+{
+	int i;
 
-        for (i = 0; i <= n; i++)
-        {
-            if (n <= 0){
-                _putchar('\n')
-            }
-            _putchar('_')
-            
-        } 
-  }
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n')
+		}
+		_putchar('_');
+	}
+}
