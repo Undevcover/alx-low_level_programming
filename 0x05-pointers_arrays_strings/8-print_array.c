a6f627c1306832d8f017c628451ceb21aecf2f1d
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-19 16:28:39 -0700

    function thst oprints n elements of an array of integers, followed by a newline

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array- function that prints n elements of an array of integers, 
+ * folloewd by a newlone
+ * @a: elements
+ * @n number of elements of the array to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	_putchar('\n')
+}
