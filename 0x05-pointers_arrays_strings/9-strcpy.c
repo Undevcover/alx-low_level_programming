1e877a0299cc1aac2d29f35c0e4f0dafc39b1ebe
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-19 16:42:11 -0700

    function that converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _atoi - function that converts a string to an integer
+ * @s: the string to be converted to an integer
+ *Return: returns an integer
+ * /
+
+int _atoi(char *s)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
