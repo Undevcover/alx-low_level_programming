1a6e5d14290ee7ddf3af570b7fd9dee58db65428
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-09-19 17:05:34 -0700

    function that generates a password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * Main -main function
+ * Return: always void
+ */
+
+int main(void)
+{
+	int i, random, sum;
+	char key[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char password[100];
+
+	srand(time(NULL));
+	sum = 0;
+	i = 0;
+
+	while (sum < (2772 - 122))
+	{
+		random = rand() % 62;
+		password[i] = key[random];
+		sum = sum + password[i];
+		i++;
+	}
+
+	random = 2772 - sum;
+	password[i] = random;
+	i++;
+	password[i] = '\0';
+	printf("%s", password);
+
+	return (0);
+}
