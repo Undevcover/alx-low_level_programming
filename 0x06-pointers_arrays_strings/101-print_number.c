b37a03858a58fdd2263f97a839948662f119a9cc
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-10-09 07:04:22 -0700

    f function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "mainn.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: Integer to print
+ */
+
+void print_number(int n)
+{
+	if (n / 10)
+	{
+		print_number(n / 10);
+		n %= 10;
+		if (n < 0)
+			n *= -1;
+	}
+
+	else
+		if (n < 0)
+		{
+			_putchar('-');
+			n *= -1;
+		}
+	_putchar(n + '0');
+}
