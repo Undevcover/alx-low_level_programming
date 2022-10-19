ed41ff29bcb6c38883b6ba91bed4db0c2089f8a1
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-10-19 14:08:47 -0700

    Write a function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function that copies n bytes from memory area src
+ * to memory area dest
+ * @dest: end point
+ * @src: starting point
+ * @n: number of bytes
+ */
+int main()
+{
+	int i;
+
+	for (i = 0;i < n;i++)
+		src[i] = dest[i];
+	return (dest);
+}
+
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0. Write a function that fills memory with a constant byte.
 
+Write a function that copies memory area
