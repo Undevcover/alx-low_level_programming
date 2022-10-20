2ccefa8a605aabf5cf3efdf066934f356feaa10a
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-10-20 05:14:06 -0700

    function that frees a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,9 +4,7 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -16,6 +14,7 @@ typedef struct list_s
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+int _strlent(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
