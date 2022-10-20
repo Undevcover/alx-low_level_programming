44f3644a29b3db229f9c236914aead494ca8ee8d
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-10-20 05:38:12 -0700

    functiion that prints a sentence before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) print(void);
+
+/**
+ * print - Function that prints a sentence before the main function is executed.
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+
+}
