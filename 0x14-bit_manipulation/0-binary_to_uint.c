d380fa49e3436bd5a7cd13f0af78a89475dfafb7
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-10-27 15:05:49 -0700

    converts binary to base 10

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,7 +24,7 @@ unsigned int binary_to_uint(const char *b)
 	/* find the length of b */
 	for (blen = 0; b[blen] != '\0'; blen++)
 		;
-	if b(len == 1 && (b[0] == '0' || b[0] == '1'))
+	if b(blen == 1 && (b[0] == '0' || b[0] == '1'))
 		return ((b[0] - 48));
 	for (i = 0; b[i] != '\0'; i++)
 	{
@@ -33,9 +33,8 @@ unsigned int binary_to_uint(const char *b)
 		for (j = len - 1; j > 0; j--)
 			pow = pow * base;
 		sum = sum + (pow * (b[i] - 48)); 
-		len--;
+		blen--;
 		pow = 1;
 	}
 	return (sum);
-}
 }
