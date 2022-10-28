74edd732790e3d76b0e3093c0af2e9a1da440d65
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-10-28 04:19:28 -0700

    function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_binary -function that prints the an int to base 2
+ * @n: The binary number to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+}
