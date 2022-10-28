1c784013232f575b15eaae73a7f084a989025cc1
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-10-28 04:57:03 -0700

    function that does some really complex stuff

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main/h"
+/**
+ * get_endianness - function that checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int i;
+	char *test;
+
+	i = 1;
+	test = (char *)&i;
+	return ((int)test[0]);
+}
