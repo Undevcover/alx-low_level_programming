e4d3487af83f9a6caead52485650b1dda3014a7a
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-02 12:21:55 -0700

    modified the code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,10 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-
-	if (n > 1)
-	{
-		print_binary(n >> 1);
-	}
-	_putchar((n & 1) + '0');
+	while (n > 0)
+        {
+                n = n/2;
+               _putchar( n&1);
+        }
 }
