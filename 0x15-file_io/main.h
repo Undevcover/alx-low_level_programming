a2f5964fcb23dfed1f3c842c5cebb00225f6d7d3
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-04 13:10:32 -0700

    function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN
+#define MAIN
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
