458bef8297137d7abfa0b6218a9e64e29e419afa
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-06 04:05:47 -0800

    print numbers between n and 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,5 +8,14 @@ void print_to_98(int n)
 {
 	int i;
 	for (i = n; i <99; i++)
-		printf("%d, ", i);
+	{
+		if (i < 98)
+		{
+			printf("%d, ", i);
+		}
+		else 
+		{
+			printf("%d\n", i);
+		}
+	}
 }
