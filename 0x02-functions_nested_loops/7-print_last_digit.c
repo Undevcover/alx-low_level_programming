bc9a3e7ae37bf6b258b9d304b5308ddebc0c5e01
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-06 02:33:42 -0800

    5 printsign.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit -function that prints the last digit
+ * of a number
+ * @n: number
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -1 * (n % 10);
+		return (n);
+	}
+	n = n % 10;
+	return (n);
+}
