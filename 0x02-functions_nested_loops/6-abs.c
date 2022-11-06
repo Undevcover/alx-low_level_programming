775f2275a768473a52170de3b3b5c3970afde021
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-06 02:17:09 -0800

    5 printsign.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs -function that returns an absolute value of an integer
+ * Return: absolute value, an int
+ */
+
+int _abs(int n)
+{
+	int n;
+	if(n < 0)
+		return (n * (-1));
+	else
+		return (n);
+}
