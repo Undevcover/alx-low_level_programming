48eb4146829350fa9ee955145a612db5aa73072a
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-06 03:42:53 -0800

    9 times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - function that prints 9 times table starting from 0
+ * return: void
+ */
+void times_table(void)
+{
+
+	int x, y, a, b;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			a = (x * y) / 10;
+			b = (x * y) % 10;
+
+			if ((x * y) > 9)
+				_putchar(a + '0');
+
+			_putchar(b + '0');
+
+			/* Don't print commas after the 10th column */
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			/* Add an extra space if printing single digits */
+			if (y == 9 && x == 0)
+				break;
+			else if (x * (y + 1) < 10)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
