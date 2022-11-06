acd718f61e77069a837950c3c95b9592aa885e34
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-06 02:38:47 -0800

    5 printsign.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,13 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		n = -1 * (n % 10);
-		_putchar(n);
+		_putchar(n + '0');
 		return (n);
 	}
 	else 
 	{
 		n = n % 10;
-		_putchar(n);
+		_putchar(n + '0');
 		return (n);
 	}
 }
