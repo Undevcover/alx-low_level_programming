541e09dd15b78863c4f31dab94670d8925758e82
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-11-06 03:52:59 -0800

    print numbers between n and 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_to_98 - prints from a given nunber to 98
+ * @n: starting point
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	int i;
+	for (i = n; i <99; i++)
+		printf("%d", i);
+}
