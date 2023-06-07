ec2e6169335f0550bf124f960068d0a8e3237527
Masturah Ghali <ronkeghali@gmail.com>
2023-06-07 04:09:37 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ * Returm - 0
+ */
+
+int main()
+{
+	int i, j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j += i;
+		}
+	}
+	printf("%d", j)
+	return 0;
+}
