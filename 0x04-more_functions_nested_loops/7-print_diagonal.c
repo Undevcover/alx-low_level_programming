dd658d9133d1be24f2bb3ff9c1caa890b75365da
Masturah Ghali <ronkeghali@gmail.com>
2023-06-11 03:02:01 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,24 @@
 
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		putchar('$');
-	}
 	int i;
 	int j;
-
-	for (i = 0; i < n; i++)
+	
+	if (n == 0)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
-		_putchar('$');
 		_putchar('\n');
 	}
-	_putchar('\n');
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
 }
